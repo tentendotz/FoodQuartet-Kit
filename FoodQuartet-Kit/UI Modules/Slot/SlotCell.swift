28009e6d488e4621e94bdcaf31f2f47dcaaa60f1
tentendotz <tenten102matsuyama@gmail.com>
2023-07-01 23:18:29 +0900

    Initial Commit

---
+++ FoodQuartet-Kit/UI Modules/Slot/SlotCell.swift
@@ -0,0 +1,23 @@
+//
+//  SlotCell.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/07/01.
+//
+
+import UIKit
+
+class SlotCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FoodQuartet-Kit/UI Modules/WebSearch/WebSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebSearchViewController.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/07/01.
+//
+
+import UIKit
+
+class WebSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
