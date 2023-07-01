d13aa848495821ddda0ae35ecd33ca2f24375e47
tentendotz <tenten102matsuyama@gmail.com>
2023-07-02 01:47:44 +0900

    feat: Add UIColor extension for converting from hex

---
+++ FoodQuartet-Kit/UI Modules/Shared/Extensions/UIColor+HexColor.swift
@@ -0,0 +1,48 @@
+//
+//  UIColor+HexColor.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/07/02.
+//
+
+import UIKit
+
+extension UIColor {
+
+    convenience init(hex: Int, alpha: CGFloat = 1) {
+        let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((hex & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat((hex & 0x0000FF) >> 0) / 255.0
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+    
+    convenience init?(hexString: String, alpha: CGFloat = 1) {
+        var hex = hexString
+        if hex.hasPrefix("#") {
+            hex.removeFirst(1)
+        }
+        if hex.lowercased().hasPrefix("0x") {
+            hex.removeFirst(2)
+        }
+        
+        let scanner = Scanner(string: hex)
+        var hexNumber: UInt64 = 0
+        guard scanner.scanHexInt64(&hexNumber) else { return nil }
+
+        switch hex.count {
+        case 6:
+            let r = CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat((hexNumber & 0x0000FF) >> 0) / 255.0
+            self.init(red: r, green: g, blue: b, alpha: alpha)
+        case 8:
+            let r = CGFloat((hexNumber & 0xFF000000) >> 24) / 255.0
+            let g = CGFloat((hexNumber & 0x00FF0000) >> 16) / 255.0
+            let b = CGFloat((hexNumber & 0x0000FF00) >> 8) / 255.0
+            let a = CGFloat((hexNumber & 0x000000FF) >> 0) / 255.0
+            self.init(red: r, green: g, blue: b, alpha: a)
+        default:
+            return nil
+        }
+    }
+}
