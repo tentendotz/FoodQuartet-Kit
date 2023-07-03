664d2adfc864fac73e86c7e78c872e99358546bf
tentendotz <tenten102matsuyama@gmail.com>
2023-07-03 23:10:28 +0900

    feat: Switch button state with specified color

--- FoodQuartet-Kit/UI Modules/Slot/SeasonButton.swift
+++ FoodQuartet-Kit/UI Modules/Slot/SeasonButton.swift
@@ -12,4 +12,20 @@ final class SeasonButton: UIButton {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    func switchState(isSelected: Bool, backgroundColor: UIColor?) {
+        self.isSelected = isSelected
+        
+        self.configurationUpdateHandler = { [unowned self] button in
+            var config = button.configuration
+            if self.isSelected {
+                config?.baseForegroundColor = UIColor.white
+                config?.baseBackgroundColor = backgroundColor
+            } else {
+                config?.baseForegroundColor = UIColor.label
+                config?.baseBackgroundColor = UIColor(named: K.Colors.tertiary)
+            }
+            button.configuration = config
+        }
+    }
 }
