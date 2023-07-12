e31bf107279789eb20d9286b484c074ef96f00b6
tentendotz <tenten102matsuyama@gmail.com>
2023-07-12 21:03:03 +0900

    feat: Add base properties to food model class

--- FoodQuartet-Kit/Models/Food.swift
+++ FoodQuartet-Kit/Models/Food.swift
@@ -9,5 +9,14 @@ import UIKit
 
 final class Food {
     
-
+    let name: String
+    let color: UIColor
+    let harvestTime: Set<String>
+    var isSelected = false
+    
+    init(name: String, color: UIColor, harvestTime: Set<String>) {
+        self.name = name
+        self.color = color
+        self.harvestTime = harvestTime
+    }
 }
