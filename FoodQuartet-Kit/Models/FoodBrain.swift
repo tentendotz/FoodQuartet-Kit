a36c5ba75aa3ce147ed51659e3d918a4cf808479
tentendotz <tenten102matsuyama@gmail.com>
2023-07-14 21:37:45 +0900

    feat: Add generate method to parse food raw data

--- FoodQuartet-Kit/Models/FoodBrain.swift
+++ FoodQuartet-Kit/Models/FoodBrain.swift
@@ -11,3 +11,25 @@ struct FoodBrain {
     
     var items = [Food]()
 }
+
+
+extension FoodBrain {
+    
+    private func generateFoods() -> [Food] {
+        var foods = [Food]()
+        let components = foodsRawData.components(separatedBy: .newlines)
+        
+        for line in components {
+            let foodData = line.split(separator: "/")
+            let name = String(foodData[0])
+            let hex = String(foodData[1])
+            let rawNumbers = foodData[3].split(separator: ",").compactMap { Int($0) }
+            
+            let hexColor = UIColor(hexString: hex)
+            
+            // TODO: - Creating food class object
+
+        }
+        return foods
+    }
+}
