b3f9eb078a7914d398fe33070f221d144dc95bfc
tentendotz <tenten102matsuyama@gmail.com>
2023-07-26 12:57:32 +0900

    add: Update food raw data with food kind information

--- FoodQuartet-Kit/Models/FoodsRawData.swift
+++ FoodQuartet-Kit/Models/FoodsRawData.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 let foodsRawData = """
-Burdock/#65523A/1,2,4,5,11,12
-Cabbage/#7ABD22/1,2,3,4,5,6,12
-Cauliflower/#F7DD78/1,2,3,11,12
-Celery/#69C95F/1,2,3,4,12
-Corn/#F1C40F/6,7,8
-Eggplant/#813E9D/7,8,9
-Pacific Saury/#3A6B95/9,10
-Potato/#A2845E/5,6,10,11
-Pumpkin/#E25809/7,8,9,10,11,12
-Sweet Potato/#BB8DCD/9,10,11,12
-Tuna/#34495E/1,11,12
-Zucchini/#0F6D5B/7,8,9
+Burdock/#65523A/veg/1,2,4,5,11,12
+Cabbage/#7ABD22/veg/1,2,3,4,5,6,12
+Cauliflower/#F7DD78/veg/1,2,3,11,12
+Celery/#69C95F/veg/1,2,3,4,12
+Corn/#F1C40F/veg/6,7,8
+Eggplant/#813E9D/veg/7,8,9
+Pacific Saury/#3A6B95/sea/9,10
+Potato/#A2845E/veg/5,6,10,11
+Pumpkin/#E25809/veg/7,8,9,10,11,12
+Sweet Potato/#BB8DCD/veg/9,10,11,12
+Tuna/#34495E/sea/1,11,12
+Zucchini/#0F6D5B/veg/7,8,9
 """
