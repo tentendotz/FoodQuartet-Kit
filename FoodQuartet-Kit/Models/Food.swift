64fb2f0bae43eecaa2ce1fa0dbbf0e0ea4dc4599
tentendotz <tenten102matsuyama@gmail.com>
2023-07-26 12:34:31 +0900

    feat: Include food kind enumeration to model class

--- FoodQuartet-Kit/Models/Food.swift
+++ FoodQuartet-Kit/Models/Food.swift
@@ -20,3 +20,37 @@ final class Food {
         self.harvestTime = harvestTime
     }
 }
+
+
+extension Food {
+    
+    // MARK: - Food Group Enumeration
+    
+    enum Kind: String {
+        case vegetable, mushroom, seafood, meat, fruit, others
+        
+        init(alias: Substring) {
+            switch alias.lowercased() {
+            case "veg": self = .vegetable
+            case "mush": self = .mushroom
+            case "fish": self = .seafood
+            case "sea": self = .seafood
+            case "meat": self = .meat
+            case "fruit": self = .fruit
+            default: self = .others
+            }
+        }
+        
+        // TODO: - Implement localization...
+        var localized: String {
+            switch self {
+            case .vegetable: return "Vegetable"
+            case .mushroom: return "Mushroom"
+            case .seafood: return "Seafood"
+            case .meat: return "Meat"
+            case .fruit: return "Fruit"
+            case .others: return "Others"
+            }
+        }
+    }
+}
