970fe5af23cb5c7d5059b39705960650b4ec2d7e
tentendotz <tenten102matsuyama@gmail.com>
2023-07-31 23:58:00 +0900

    feat: Add UIView extension with IBInspectable properties

---
+++ FoodQuartet-Kit/Shared/Extensions/UIView+IBInspectable.swift
@@ -0,0 +1,76 @@
+//
+//  UIView+IBInspectable.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/07/31.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            guard let cgColor = layer.shadowColor else { return nil }
+            return UIColor(cgColor: cgColor)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
