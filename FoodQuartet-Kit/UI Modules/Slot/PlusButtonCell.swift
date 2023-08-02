d6f1e04bf86ad86c3e5e2d2f2d0dda0b7a3e9b57
tentendotz <tenten102matsuyama@gmail.com>
2023-08-03 01:00:00 +0900

    add: Create the plus button cell class

---
+++ FoodQuartet-Kit/UI Modules/Slot/PlusButtonCell.swift
@@ -0,0 +1,21 @@
+//
+//  PlusButtonCell.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/08/04.
+//
+
+import UIKit
+
+final class PlusButtonCell: UITableViewCell {
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
