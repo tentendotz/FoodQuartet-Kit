1c684fd69b5d1c4615d271a89a6f217092b2773f
tentendotz <tenten102matsuyama@gmail.com>
2023-08-04 18:30:18 +0900

    feat: Add a button as subview at the center of the cell

--- FoodQuartet-Kit/UI Modules/Slot/PlusButtonCell.swift
+++ FoodQuartet-Kit/UI Modules/Slot/PlusButtonCell.swift
@@ -9,13 +9,35 @@ import UIKit
 
 final class PlusButtonCell: UITableViewCell {
     
+    let button = UIButton()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        contentView.backgroundColor = UIColor(named: K.Colors.primary)
+        createSubview(in: contentView)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+
+extension PlusButtonCell {
+    
+    private func createSubview(in contentView: UIView) {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(button)
+        
+        let inset = CGFloat(50)
+        NSLayoutConstraint.activate([
+            contentView.heightAnchor.constraint(equalToConstant: 50),
+            button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            button.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            button.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset)
+        ])
+    }
+    
 }
