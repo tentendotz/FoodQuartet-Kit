e77505fa41e0c2d2bf62aa7304518b422d169c8d
tentendotz <tenten102matsuyama@gmail.com>
2023-09-04 12:18:21 +0900

    feat: Include localization method to Constants file

--- FoodQuartet-Kit/Shared/Constants.swift
+++ FoodQuartet-Kit/Shared/Constants.swift
@@ -52,12 +52,45 @@ extension K {
     
     /// Localization = L10n
     enum L10n {
-                
+        
+        // Food Kind
+        static let vegetable = L10n.localized("type_vegetable")
+        static let seafood = L10n.localized("type_seafood")
+        static let mushroom = L10n.localized("type_mushroom")
+        static let meat = L10n.localized("type_meat")
+        static let fruit = L10n.localized("type_fruit")
+        static let others = L10n.localized("type_others")
+        
         // Four Seasons Filter
-        static let noFilter = "No Filter"
-        static let spring = "Spring (MAR. APR. MAY)"
-        static let summer = "Summer (JUN. JUL. AUG.)"
-        static let fall = "Fall (SEP. OCT. NOV.)"
-        static let winter = "Winter (DEC. JAN. FEB.)"
+        static let noFilter = L10n.localized("type_no_filter")
+        static let spring = L10n.localized("type_spring")
+        static let summer = L10n.localized("type_summer")
+        static let fall = L10n.localized("type_fall")
+        static let winter = L10n.localized("type_winter")
+        
+        // Action Button
+        static let back = L10n.localized("action_back")
+        static let delete = L10n.localized("action_delete")
+        static let reset = L10n.localized("action_reset")
+        static let ok = L10n.localized("action_ok")
+        static let cancel = L10n.localized("action_cancel")
+        
+        // SlotVC
+        static let noData = L10n.localized("title_no_data")
+        static let tapPlusButton = L10n.localized("message_tap_plusButton")
+        static let notFound = L10n.localized("title_not_found")
+        static let changeFilter = L10n.localized("message_change_filter")
+        static let plusButtonDisabled = L10n.localized("message_plusButton_disabled")
+        static let allPinned = L10n.localized("title_all_pinned")
+        static let deselectItem = L10n.localized("message_deselect_item")
+        static let askReset = L10n.localized("title_ask_reset")
+        
+                
+        // MARK: - Localization Helper
+        
+        private static func localized(_ key: String, _ args: CVarArg...) -> String {
+            let format = NSLocalizedString(key, comment: "")
+            return String(format: format, arguments: args)
+        }
     }
 }
