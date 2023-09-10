c0fac19645853602676f521067bc68dd556a6fad
tentendotz <tenten102matsuyama@gmail.com>
2023-09-10 23:40:00 +0900

    feat: Add configuration method for web search cell

--- FoodQuartet-Kit/UI Modules/WebSearch/WebSearchCell.swift
+++ FoodQuartet-Kit/UI Modules/WebSearch/WebSearchCell.swift
@@ -17,3 +17,26 @@ final class WebSearchCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
+extension WebSearchCell {
+    
+    func configureCell(title: String, secondaryTitle: String) {
+        var contentConfig = defaultContentConfiguration()
+        contentConfig.text = title
+        contentConfig.textProperties.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        contentConfig.textProperties.color = UIColor.label
+        contentConfig.textProperties.alignment = .center
+
+        contentConfig.secondaryText = secondaryTitle
+        contentConfig.secondaryTextProperties.font = UIFont.systemFont(ofSize: 14)
+        contentConfig.secondaryTextProperties.color = UIColor.tertiaryLabel
+        contentConfig.secondaryTextProperties.alignment = .center
+
+        var backgroundConfig = UIBackgroundConfiguration.listPlainCell()
+        backgroundConfig.backgroundColor = UIColor(named: K.Colors.secondary)
+        
+        self.contentConfiguration = contentConfig
+        self.backgroundConfiguration = backgroundConfig
+    }
+}
