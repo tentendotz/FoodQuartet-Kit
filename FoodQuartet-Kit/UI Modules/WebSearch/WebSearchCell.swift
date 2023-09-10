44cf43076440b7e8cffaa14ded94db23fbd3f13c
tentendotz <tenten102matsuyama@gmail.com>
2023-09-10 23:29:32 +0900

    add: Create web search cell class

---
+++ FoodQuartet-Kit/UI Modules/WebSearch/WebSearchCell.swift
@@ -0,0 +1,19 @@
+//
+//  WebSearchCell.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/09/10.
+//
+
+import UIKit
+
+final class WebSearchCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
