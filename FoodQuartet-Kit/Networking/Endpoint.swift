17d105a582b3f2eea19b6162ec5eb1dd4e0f7c85
tentendotz <tenten102matsuyama@gmail.com>
2023-10-09 03:00:00 +0900

    feat: Add URL component properties to endpoint struct

--- FoodQuartet-Kit/Networking/Endpoint.swift
+++ FoodQuartet-Kit/Networking/Endpoint.swift
@@ -9,4 +9,16 @@ import Foundation
 
 struct Endpoint {
     
+    let queryItems: [URLQueryItem]?
+    
+    // https://www.google.com/search?q=queryItem
+    var url: URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "www.google.com"
+        urlComponents.path = "/search"
+        urlComponents.queryItems = queryItems
+        
+        return urlComponents.url
+    }
 }
