9e6692bd472cefd8b8cdab3c6e1f43c9ae4174c6
tentendotz <tenten102matsuyama@gmail.com>
2023-11-06 21:18:13 +0900

    add: Create the month cell class

---
+++ FoodQuartet-Kit/UI Modules/MonthPicker/MonthCell.swift
@@ -0,0 +1,21 @@
+//
+//  MonthCell.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/11/06.
+//
+
+import UIKit
+
+final class MonthCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
