4c5f10358cd1fc72836ecb108817ca7cd06cd895
tentendotz <tenten102matsuyama@gmail.com>
2023-11-08 21:12:06 +0900

    add: Create the month header class

---
+++ FoodQuartet-Kit/UI Modules/MonthPicker/MonthHeader.swift
@@ -0,0 +1,19 @@
+//
+//  MonthHeader.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/11/08.
+//
+
+import UIKit
+
+final class MonthHeader: UITableViewHeaderFooterView {
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
