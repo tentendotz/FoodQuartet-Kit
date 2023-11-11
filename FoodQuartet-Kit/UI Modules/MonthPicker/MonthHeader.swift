ce4728e0736e705685a169604385558ea54792fd
tentendotz <tenten102matsuyama@gmail.com>
2023-11-11 22:14:47 +0900

    feat: Create auto layout constraints programmatically

--- FoodQuartet-Kit/UI Modules/MonthPicker/MonthHeader.swift
+++ FoodQuartet-Kit/UI Modules/MonthPicker/MonthHeader.swift
@@ -14,9 +14,35 @@ final class MonthHeader: UITableViewHeaderFooterView {
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
+        createSubview(in: contentView)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+extension MonthHeader {
+    
+    private func createSubview(in contentView: UIView) {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        
+        let size = CGFloat(30)
+        let inset = CGFloat(6)
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: size),
+            imageView.heightAnchor.constraint(equalToConstant: size),
+            imageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: -inset),
+            imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: inset),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
 }
