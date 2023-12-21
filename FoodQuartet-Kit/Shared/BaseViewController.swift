a237ab81e8274c1e657a151fe5b8810c2f346fb3
tentendotz <tenten102matsuyama@gmail.com>
2023-12-21 20:00:00 +0900

    add: Create a base view controller class as the superclass

---
+++ FoodQuartet-Kit/Shared/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  FoodQuartet-Kit
+//
+//  Created by tetsuta matsuyama on 2023/12/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
