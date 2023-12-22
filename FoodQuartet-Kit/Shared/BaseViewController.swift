eb93d983c9ddb15c0b47dacaf79422360dcaaea2
tentendotz <tenten102matsuyama@gmail.com>
2023-12-22 21:36:41 +0900

    refactor: Replace the alert extension with a method in superclass

--- FoodQuartet-Kit/Shared/BaseViewController.swift
+++ FoodQuartet-Kit/Shared/BaseViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
+    // MARK: - Common Helpers
+    
+    func displayAlert(title: String, message: String?, actions: [UIAlertAction], style: UIAlertController.Style = .alert) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        present(alert, animated: true)
+    }
 }
